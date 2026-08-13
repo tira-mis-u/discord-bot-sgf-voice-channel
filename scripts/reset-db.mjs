913cc084ccf7a8ebfb75bcedd9a8f95f6559ffb8
Teara Misher <trisnosleep@gmail.com>
2026-08-13 13:04:10 +0700

    Delete scripts directory

--- scripts/reset-db.mjs
+++
@@ -1,9 +0,0 @@
-import fs from 'node:fs';
-import path from 'node:path';
-
-const dbFile = process.env.DB_FILE || './data/sgf.sqlite';
-const absolute = path.resolve(dbFile);
-for (const file of [absolute, `${absolute}-wal`, `${absolute}-shm`]) {
-  if (fs.existsSync(file)) fs.unlinkSync(file);
-}
-console.log(`Removed ${absolute}`);
